964f4d7c4e4f1c5ca968a3e195664f95ea2b7e6d
FrancisFlow <iamngigikaranja1@aol.com>
2021-11-29 22:40:14 +0300

    Created a promise to get user data from the githubApi

--- searchGithub/src/app/git-service.service.ts
+++ searchGithub/src/app/git-service.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitServiceService {
 
-  constructor() { }
+  user: User;
+
+
+  getUserData(userName:string){
+    interface userApiResponse{
+      followers:string;
+      following:string;
+      login: string;
+      avatar_url:string;
+    }
+    let promise = new Promise<void>((resolve, reject)=>{
+      this.http.get<userApiResponse>(`https://api.github.com/users/${userName}`).toPromise().then(response=>{
+        this.user.followers = response.followers;
+        this.user.following = response.following;
+        this.user.avatar_url= response.avatar_url;
+        this.user.login = response.login;
+
+        resolve()
+      },
+      error=>{
+        this.user.login='User not found'
+        console.log("Something went wrong")
+        reject(error)
+      })
+
+    })
+    return promise;
+  }
+
+ 
+
+  constructor(private http:HttpClient) { 
+    this.user = new User( "", "", "", "");
+  }
 }
