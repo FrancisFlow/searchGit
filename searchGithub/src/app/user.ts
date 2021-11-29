bb9cfb2d2ffd4880acc04667e8d4a159e5ab010a
FrancisFlow <iamngigikaranja1@aol.com>
2021-11-29 22:33:05 +0300

    Created a class for user

--- searchGithub/src/app/user.ts
+++ searchGithub/src/app/user.ts
@@ -1,2 +1,10 @@
 export class User {
+    constructor(
+        public following:string,
+        public followers:string,
+        public login: string,
+        public avatar_url:string
+    ){
+
+    }
 }
