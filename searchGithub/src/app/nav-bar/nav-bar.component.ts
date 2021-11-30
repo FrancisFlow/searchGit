b81d50797782af3468d5e81c44c8d1059b269be9
FrancisFlow <iamngigikaranja1@aol.com>
2021-12-01 01:43:40 +0300

    Created searchForUser function to search for github users when a userName is entered

--- searchGithub/src/app/nav-bar/nav-bar.component.ts
+++ searchGithub/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../user';
+import { GitServiceService } from '../git-service.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gitService:GitServiceService) { }
+
+  user: User;
+  userName: string;
+
+  // function to fetch 
+
+  searchForUser()
+  {
+    this.user = this.gitService.user
+    this.gitService.getUserData(this.userName);
+
+  }
 
   ngOnInit(): void {
   }
