a6846a809af647d9a962531219cba965f9db36ca
FrancisFlow <iamngigikaranja1@aol.com>
2021-12-02 12:12:49 +0300

    Implemented a custom directive to highlight public repositories if greater than 20

--- searchGithub/src/app/repository-highlight.directive.ts
+++ searchGithub/src/app/repository-highlight.directive.ts
@@ -1,10 +1,13 @@
 import { Directive } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appRepositoryHighlight]'
 })
 export class RepositoryHighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { 
+    this.elem.nativeElement.style.background= 'turquoise'
+  }
 
 }
